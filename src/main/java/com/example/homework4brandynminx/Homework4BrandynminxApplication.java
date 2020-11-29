b75d56000c1a9edcab8971387ed72314e2ed4123
brandynminx <bnminx@uh.edu>
2020-11-29 04:43:04 -0600

    Initial commit

---
+++ src/main/java/com/example/homework4brandynminx/Homework4BrandynminxApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework4brandynminx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework4BrandynminxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework4BrandynminxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homework4brandynminx/Homework4BrandynminxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework4brandynminx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework4BrandynminxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
